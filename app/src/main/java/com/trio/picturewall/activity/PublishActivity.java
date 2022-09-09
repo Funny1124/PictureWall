6a93c2ff68459c67db928143c8df05ffaeb732bb
yangrf66 <2385673315@qq.com>
2022-09-09 23:44:13 +0800

    更新布局

---
+++ app/src/main/java/com/trio/picturewall/activity/PublishActivity.java
@@ -0,0 +1,16 @@
+package com.trio.picturewall.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.trio.picturewall.R;
+
+public class PublishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+    }
+}
--- app/src/main/java/com/trio/picturewall/ui/share/ShareFragment.java
+++ app/src/main/java/com/trio/picturewall/ui/share/ShareFragment.java
@@ -2,6 +2,7 @@ package com.trio.picturewall.ui.share;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,10 +14,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.trio.picturewall.R;
+import com.trio.picturewall.activity.PublishActivity;
+import com.trio.picturewall.databinding.FragmentShareBinding;
+import com.trio.picturewall.widget.CircleButton;
 
 public class ShareFragment extends Fragment {
 
     private ShareViewModel mViewModel;
+    private FragmentShareBinding binding;
 
     public static ShareFragment newInstance() {
         return new ShareFragment();
@@ -25,8 +30,21 @@ public class ShareFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_share, container, false);
-    }
+        ShareViewModel shareViewModel =
+                new ViewModelProvider(this).get(ShareViewModel.class);
+
+        binding = FragmentShareBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final CircleButton circleButton = binding.upload;
+        circleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PublishActivity.class));
+            }
+        });
+
+        return root;    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
