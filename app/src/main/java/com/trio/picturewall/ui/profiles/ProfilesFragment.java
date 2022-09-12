bffa6d663d4f4a57293ae7ec085bd8b646376dab
Cyan <204267667@qq.com>
2022-09-12 16:55:43 +0800

    添加头像点击弹窗,跳转修改信息，登录界面

--- app/src/main/java/com/trio/picturewall/ui/profiles/ProfilesFragment.java
+++ app/src/main/java/com/trio/picturewall/ui/profiles/ProfilesFragment.java
@@ -1,25 +1,30 @@
 package com.trio.picturewall.ui.profiles;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.trio.picturewall.R;
+import com.trio.picturewall.activity.AlterActivity;
+import com.trio.picturewall.activity.LoginActivity;
 import com.trio.picturewall.databinding.FragmentProfilesBinding;
-import com.trio.picturewall.ui.share.ShareViewModel;
+import com.trio.picturewall.information.LoginData;
 
 public class ProfilesFragment extends Fragment {
 
     private ProfilesViewModel profilesViewModel;
     private FragmentProfilesBinding binding;
+
     public static ProfilesFragment newInstance() {
         return new ProfilesFragment();
     }
@@ -27,9 +32,17 @@ public class ProfilesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        profilesViewModel=new ViewModelProvider(this).get(ProfilesViewModel.class);
+        profilesViewModel = new ViewModelProvider(this).get(ProfilesViewModel.class);
         binding = FragmentProfilesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.mineUserIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showListDialog();
+            }
+        });
+
         return root;
     }
 
@@ -40,4 +53,48 @@ public class ProfilesFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    /**
+     * 普通列表dialog
+     */
+    private void showListDialog() {
+        final String[] listItems = new String[]{"修改信息", "退出登录"};
+
+        AlertDialog.Builder listDialog = new AlertDialog.Builder(getContext());
+        listDialog.setTitle(getString(R.string.dialog_list_text));
+        listDialog.setIcon(R.mipmap.ic_launcher_round);
+
+    /*
+        设置item 不能用setMessage()
+        用setItems
+        items : listItems[] -> 列表项数组
+        listener -> 回调接口
+    */
+        listDialog.setItems(listItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0://修改信息
+                        Log.v("Avatar Dialog:", listItems[which]);
+                        startActivity(new Intent(getActivity(), AlterActivity.class));
+                        break;
+                    case 1://退出登录
+                        Log.v("Avatar Dialog:", listItems[which]);
+                        LoginData.loginUser = null;
+                        startActivity(new Intent(getActivity(), LoginActivity.class));
+                        break;
+                }
+            }
+        });
+
+        //设置按钮
+        listDialog.setPositiveButton(getString(R.string.dialog_btn_confirm_text)
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        listDialog.create().show();
+    }
 }
