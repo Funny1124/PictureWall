4a2600458d9a9e0ee2047feb1273cc3d74c3eab5
Cyan <204267667@qq.com>
2022-10-06 15:03:00 +0800

    DetailActivity长按保存图片（单张，全部）

--- app/src/main/java/com/trio/picturewall/activity/DetailActivity.java
+++ app/src/main/java/com/trio/picturewall/activity/DetailActivity.java
@@ -1,8 +1,14 @@
 package com.trio.picturewall.activity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.NetworkOnMainThreadException;
 import android.util.Log;
 import android.view.Gravity;
@@ -20,6 +26,8 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.Target;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.trio.picturewall.Http.Api;
@@ -31,6 +39,8 @@ import com.trio.picturewall.entity.MyPosts;
 import com.trio.picturewall.information.LoginData;
 import com.trio.picturewall.responseBody.ResponseBody;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -48,7 +58,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
     public static MyPosts post;
     TextView user_name;
     TextView post_title;
@@ -112,9 +122,9 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
                 }
                 break;
 
-            case R.id.image_detail:
-                //TODO
-                break;
+//            case R.id.image_detail://图片监听
+//                //
+//                break;
             case R.id.btn_like:
                 if (!post.getHasLike()) {//detail.getHasFocus() == false：未关注->已关注
                     like();
@@ -166,6 +176,19 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    @Override
+    public boolean onLongClick(View view) {
+        switch (view.getId()) {
+            case R.id.image_detail://图片监听
+                Log.d("Detail", "onLongClick");
+                showListDialog();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
     public void initView() {
         user_name = findViewById(R.id.user_name);
         post_title = findViewById(R.id.post_title);
@@ -179,7 +202,8 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         forward_image = findViewById(R.id.forward_image);
         next_image = findViewById(R.id.next_image);
         focus.setOnClickListener(this);
-        photo.setOnClickListener(this);
+//        photo.setOnClickListener(this);
+        photo.setOnLongClickListener(this);
         cancel.setOnClickListener(this);
         btn_like.setOnClickListener(this);
         btn_collect.setOnClickListener(this);
@@ -689,5 +713,122 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
     private void refreshData() {
         getComment();
+    }
+
+    /**
+     * 普通列表dialog
+     */
+    private void showListDialog() {
+        final String[] listItems = new String[]{"保存当前图片", "保存所有图片"};
+
+        AlertDialog.Builder listDialog = new AlertDialog.Builder(DetailActivity.this);
+        listDialog.setTitle("保存图片");
+        listDialog.setIcon(R.mipmap.seraphine);
+
+    /*
+        设置item 不能用setMessage()
+        用setItems
+        items : listItems[] -> 列表项数组
+        listener -> 回调接口
+    */
+        listDialog.setItems(listItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0://保存当前图片
+                        Log.v("Save Image :", listItems[which]);
+                        new Thread() {
+                            public void run() {
+                                Log.e("图片", "开始下载");
+                                try {
+                                    Bitmap myBitmap = Glide.with(DetailActivity.this)
+                                            .asBitmap()
+                                            .skipMemoryCache(true)//跳过内存缓存
+                                            .diskCacheStrategy(DiskCacheStrategy.NONE)//不缓冲disk硬盘中
+                                            .load(post.getImageUrlList()[i])
+                                            .submit(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).get();
+                                    Bitmap bitmap = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight());
+                                    Log.e("图片", bitmap.toString());
+                                    saveImageToGallery(bitmap);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }.start();
+                        break;
+                    case 1://保存所有图片
+                        Log.v("Save Image :", listItems[which]);
+                        new Thread() {
+                            public void run() {
+                                Log.e("图片", "开始下载");
+                                try {
+                                    for (int index = 0; index < post.getImageUrlList().length; index++) {
+                                        Bitmap myBitmap = Glide.with(DetailActivity.this)
+                                                .asBitmap()
+                                                .skipMemoryCache(true)//跳过内存缓存
+                                                .diskCacheStrategy(DiskCacheStrategy.NONE)//不缓冲disk硬盘中
+                                                .load(post.getImageUrlList()[index])
+                                                .submit(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).get();
+                                        Bitmap bitmap = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight());
+                                        Log.e("图片", bitmap.toString());
+                                        saveImageToGallery(bitmap);
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }.start();
+                        break;
+                }
+            }
+        });
+
+        //设置按钮
+        listDialog.setPositiveButton(getString(R.string.dialog_btn_confirm_text)
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        listDialog.create().show();
+    }
+
+    public void saveImageToGallery(Bitmap bmp) {
+        if (bmp == null) {
+            Log.e("TAG", "bitmap---为空");
+            return;
+        }
+        String galleryPath = Environment.getExternalStorageDirectory()
+                + File.separator + Environment.DIRECTORY_DCIM
+                + File.separator + "Camera" + File.separator;
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(galleryPath, fileName);
+        try {
+            if (!Objects.requireNonNull(file.getParentFile()).exists()) {
+                if (!file.getParentFile().mkdirs()) {
+                    Log.e("图片", "创建文件失败");
+                }
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            //通过io流的方式来压缩保存图片
+            boolean isSuccess = bmp.compress(Bitmap.CompressFormat.JPEG, 60, fos);
+            fos.flush();
+            fos.close();
+            //保存图片后发送广播通知更新数据库
+            Uri uri = Uri.fromFile(file);
+            DetailActivity.this.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+            if (isSuccess) {
+                Log.e("TAG", "图片保存成功 保存在:" + file.getPath());
+            } else {
+                Log.e("TAG", "图片保存失败");
+            }
+        } catch (IOException e) {
+            Log.e("TAG", "保存图片找不到文件夹");
+            e.printStackTrace();
+        }
     }
 }
